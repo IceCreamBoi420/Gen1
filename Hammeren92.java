5fccae4fd21dd8a3d0ba0466e5a25db8c0291b96
marcusgarner <marcus.garner9678@gmail.com>
2022-11-17 15:16:38 +0100

    Hejsa

---
+++ Hammeren92.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class Hammeren92 {
+    private int rage;
+    private int height;
+    private List<String> jokes;
+
+    public Hammeren92(int rage, int height) {
+        this.rage = rage;
+        this.height = height;
+
+    }
+
+    public int getRage() {
+        return rage;
+    }
+
+    public void setRage(int rage) {
+        this.rage = rage;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    
+}
